702354bec702ac60fab507460950d2a8124590b0
Md Rashed Ali <tools.rashed@gmail.com>
2023-06-05 11:54:28 +0600

    addig some components file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+import Home from "./Components/Home/Home";
+import Nav from "./Components/Nav/Nav";
+import "./App.css";
+
 function App() {
   return (
     <>
       <h1>Portfolio is Comming Soon!!</h1>
+      <div>
+        <h2>Components</h2>
+        <Home />
+        <Nav />
+      </div>
     </>
   );
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className='container home-container'>
+      <h1 className='title'>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
