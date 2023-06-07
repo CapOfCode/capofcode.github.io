2364066dfb5e2657d1e70fba5eb4e15aa171d15d
Md Rashed Ali <tools.rashed@gmail.com>
2023-06-07 12:37:35 +0600

    icon added to nav menu for mobile and tab

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,27 +1,49 @@
 import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillPersonFill } from "react-icons/bs";
+import { GiSkills } from "react-icons/gi";
+import { MdWorkHistory } from "react-icons/md";
+import { AiFillMessage } from "react-icons/ai";
 import "./Nav.css";
 
 const Nav = () => {
   return (
-    <div className='nav'>
-      <ul>
-        <li>
-          <a href='#Home'>Home</a>
-        </li>
-        <li>
-          <a href='#About'>About</a>
-        </li>
-        <li>
-          <a href='#Skills'>Skills</a>
-        </li>
-        <li>
-          <a href='#Projects'>Projects</a>
-        </li>
-        <li>
-          <a href='#Contact'>Contact</a>
-        </li>
-      </ul>
-    </div>
+    <nav>
+      <a href='#Home'>
+        <span className='laptop-menu'>Home</span>
+        <span className='mobile-menu'>
+          <AiFillHome />
+        </span>
+      </a>
+
+      <a href='#About'>
+        <span className='laptop-menu'>About</span>
+        <span className='mobile-menu'>
+          <BsFillPersonFill />
+        </span>
+      </a>
+
+      <a href='#Skills'>
+        <span className='laptop-menu'>Skills</span>
+        <span className='mobile-menu'>
+          <GiSkills />
+        </span>
+      </a>
+
+      <a href='#Projects'>
+        <span className='laptop-menu'>Projects</span>
+        <span className='mobile-menu'>
+          <MdWorkHistory />
+        </span>
+      </a>
+
+      <a href='#Contact'>
+        <span className='laptop-menu'>Contact</span>
+        <span className='mobile-menu'>
+          <AiFillMessage />
+        </span>
+      </a>
+    </nav>
   );
 };
 
