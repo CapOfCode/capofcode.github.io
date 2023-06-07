6d1a607fbc2eed3838ce97426e2fd56e5c01f714
Md Rashed Ali <tools.rashed@gmail.com>
2023-06-07 15:27:03 +0600

    home social icon and scrol down add and css

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { BsYoutube } from "react-icons/bs";
+import { FaTwitterSquare } from "react-icons/fa";
 import "./Home.css";
 
 const Home = () => {
@@ -7,6 +11,26 @@ const Home = () => {
       <div className='container home-container'>
         <header>
           <h1>Home</h1>
+
+          <div className='header-socials'>
+            <a href='##' target='_blank'>
+              <BsLinkedin />
+            </a>
+            <a href='##' target='_blank'>
+              <FaGithubSquare />
+            </a>
+            <a href='##' target='_blank'>
+              <BsYoutube />
+            </a>
+            <a href='##' target='_blank'>
+              <FaTwitterSquare />
+            </a>
+          </div>
+          <div className='scroll-down'>
+            <a href='##'>
+              <span>Scrol Down &raquo; &raquo;</span>
+            </a>
+          </div>
         </header>
       </div>
     </section>
