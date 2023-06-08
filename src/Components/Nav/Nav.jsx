7a9878b390de59386f0c0754e1b30428eadc43be
Md Rashed Ali <tools.rashed@gmail.com>
2023-06-08 13:19:40 +0600

    creating hover and active function for laptop and mobile menu

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
 import { GiSkills } from "react-icons/gi";
@@ -7,42 +7,79 @@ import { AiFillMessage } from "react-icons/ai";
 import "./Nav.css";
 
 const Nav = () => {
+  const [activeNow, setActiveNow] = useState("#Home");
   return (
     <nav>
-      <div className='logo'>
+      <div
+        className='logo'
+        onClick={() => {
+          window.location.href = "#Home";
+        }}>
         <h2>CapOfCode</h2>
       </div>
-      <a href='#Home'>
+      <a
+        href='#Home'
+        className={activeNow === "#Home" ? "active" : ""}
+        onClick={() => {
+          setActiveNow("#Home");
+        }}>
         <span className='laptop-menu'>Home</span>
-        <span className='mobile-menu'>
+        <span
+          className={`mobile-menu ${activeNow === "#Home" ? "active" : ""}`}>
           <AiFillHome />
         </span>
       </a>
 
-      <a href='#About'>
+      <a
+        href='#About'
+        className={activeNow === "#About" ? "active" : ""}
+        onClick={() => {
+          setActiveNow("#About");
+        }}>
         <span className='laptop-menu'>About</span>
-        <span className='mobile-menu'>
+        <span
+          className={`mobile-menu ${activeNow === "#About" ? "active" : ""}`}>
           <BsFillPersonFill />
         </span>
       </a>
 
-      <a href='#Skills'>
+      <a
+        href='#Skills'
+        className={activeNow === "#Skills" ? "active" : ""}
+        onClick={() => {
+          setActiveNow("#Skills");
+        }}>
         <span className='laptop-menu'>Skills</span>
-        <span className='mobile-menu'>
+        <span
+          className={`mobile-menu ${activeNow === "#Skills" ? "active" : ""}`}>
           <GiSkills />
         </span>
       </a>
 
-      <a href='#Projects'>
+      <a
+        href='#Projects'
+        className={activeNow === "#Projects" ? "active" : ""}
+        onClick={() => {
+          setActiveNow("#Projects");
+        }}>
         <span className='laptop-menu'>Projects</span>
-        <span className='mobile-menu'>
+        <span
+          className={`mobile-menu ${
+            activeNow === "#Projects" ? "active" : ""
+          }`}>
           <MdWorkHistory />
         </span>
       </a>
 
-      <a href='#Contact'>
+      <a
+        href='#Contact'
+        className={activeNow === "#Contact" ? "active" : ""}
+        onClick={() => {
+          setActiveNow("#Contact");
+        }}>
         <span className='laptop-menu'>Contact</span>
-        <span className='mobile-menu'>
+        <span
+          className={`mobile-menu ${activeNow === "#Contact" ? "active" : ""}`}>
           <AiFillMessage />
         </span>
       </a>
