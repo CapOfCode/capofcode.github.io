45b9eeed54531e38bd0e2c0ac2ea56715b2f6f8b
Md Rashed Ali <tools.rashed@gmail.com>
2023-06-10 00:34:48 +0600

    adding skill name and project count to div

--- src/Components/Skills/SkillCard.jsx
+++ src/Components/Skills/SkillCard.jsx
@@ -5,8 +5,12 @@ const SkillCard = (props) => {
   const { image } = props;
   return (
     <div className='card'>
-      <h1>HTML</h1>
-      <h1 className='project-count'>Project</h1>
+      <h1>
+        <span className='skill-name'>Javascript</span>
+      </h1>
+      <h1 className='project-count'>
+        <span className='projects-done'>Project: 20</span>
+      </h1>
       <div className='icon-card'>
         <div className='card-image'>
           <img src={image} alt='' />
