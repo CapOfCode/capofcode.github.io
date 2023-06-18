ae56a036a6a0b2b6a6f7e492f749a9507d01e663
Md Rashed Ali <tools.rashed@gmail.com>
2023-06-18 15:22:07 +0600

    Footer Section Add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { BsYoutube } from "react-icons/bs";
+import { FaTwitterSquare } from "react-icons/fa";
+
 import "./Footer.css";
 
 const Footer = () => {
@@ -6,7 +11,29 @@ const Footer = () => {
     <section id='footer'>
       <footer>
         <div className='container footer-container'>
-          <h1>Footer</h1>
+          <div className='footer-socials'>
+            <a href='https://www.linkedin.com/in/capofcode/' target='_blank'>
+              <div className='footer-icon'>
+                <BsLinkedin className='linkedin-icon' />
+              </div>
+            </a>
+            <a href='https://github.com/CapOfCode' target='_blank'>
+              <div className='footer-icon'>
+                <FaGithubSquare className='github-icon' />
+              </div>
+            </a>
+            <a href='https://www.youtube.com/@capofcode' target='_blank'>
+              <div className='footer-icon'>
+                <BsYoutube className='youtube-icon' />
+              </div>
+            </a>
+            <a href='https://twitter.com/capofcode' target='_blank'>
+              <div className='footer-icon'>
+                <FaTwitterSquare className='twitter-icon' />
+              </div>
+            </a>
+          </div>
+          <p>Copyright &copy; Rashed</p>
         </div>
       </footer>
     </section>
