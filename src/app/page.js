9762f16440b3ec8d7c40a98573837f9ef0728720
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-12 00:22:18 +0600

    section created

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,31 @@
-import NavBar from '@/components/ui/nav/navbar'
-import { Button } from '../components/ui/material_tailwind'
-import Image from 'next/image'
-import { ThemeSwitcher } from '@/Theme/ThemeSwitcher'
-import Banner from '@/components/ui/banner/Banner'
-import SectionTitle from '@/components/ui/shared/SectionTitle'
+import NavBar from "@/components/ui/nav/navbar";
+import { Button } from "../components/ui/material_tailwind";
+import Image from "next/image";
+import { ThemeSwitcher } from "@/Theme/ThemeSwitcher";
+import Banner from "@/components/ui/banner/Banner";
+import SectionTitle from "@/components/ui/shared/SectionTitle";
 
 export default function Home() {
   return (
     <main>
-     <NavBar></NavBar>
-     <Banner></Banner>
-     
-    <SectionTitle></SectionTitle>
+      <section id="home">
+        <NavBar></NavBar>
+        <Banner></Banner>
+      </section>
+      <section id="about">
+        <SectionTitle></SectionTitle>
+      </section>
+
+      <section id="projects">
+        <SectionTitle></SectionTitle>
+      </section>
+
+      <section id="contact">
+        <SectionTitle></SectionTitle>
+      </section>
+      <section id="footer">
+        <p>Footer</p>
+      </section>
     </main>
-  )
+  );
 }
