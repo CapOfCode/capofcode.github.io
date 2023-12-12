467f9a959cd4706016451d46517d5a2ab0d1c0b7
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-12 15:59:07 +0600

    navbar responsvie

--- src/components/ui/nav/NavBar.js
+++ src/components/ui/nav/NavBar.js
@@ -1,67 +1,25 @@
-"use client";
 import Link from "next/link";
 import { Avatar } from "../material_tailwind";
 import { ThemeSwitcher } from "@/Theme/ThemeSwitcher";
-import { useEffect, useState } from "react";
 
 const NavBar = () => {
-  const [activeNav, setActiveNav] = useState("#home");
-
-  useEffect(() => {
-    setActiveNav("#home");
-  }, []);
-
   return (
     <div className="bg-dark shadow-shadowColor">
       <nav className="max-w-[1240px]  px-[20px] flex justify-between items-center py-3 mx-auto">
         <div className="logo">
-          <Link
-            href="#home"
-            onClick={() => {
-              setActiveNav("#home");
-            }}
-            className={`${activeNav ? "text-xl text-accent" : "text-xl"}`}
-          >
+          <Link href="#home">
             <Avatar
               src="https://i.ibb.co/sRJYQrx/profile-pic-2.png"
               alt="avatar"
-              className="border-2 border-accent rounded-full shadow-lightDarker shadow-customShadow hover:shadow-customShadow focus:shadow-customShadow hover:shadow-accent  focus:shadow-accent transition-all ease-in-out"
+              className="h-8 w-8 lg:w-12 lg:h-12 border-2 border-accent rounded-full shadow-lightDarker shadow-customShadow hover:shadow-customShadow focus:shadow-customShadow hover:shadow-accent  focus:shadow-accent transition-all ease-in-out"
             />
           </Link>
         </div>
-        <div className="menu text-light flex justify-center gap-3 items-center">
-          <Link
-            href="#home"
-            onClick={() => {
-              setActiveNav("#home");
-            }}
-          >
-            Home
-          </Link>
-          <Link
-            href="#about"
-            onClick={() => {
-              setActiveNav("#about");
-            }}
-          >
-            About
-          </Link>
-          <Link
-            href="#projects"
-            onClick={() => {
-              setActiveNav("#projects");
-            }}
-          >
-            Project
-          </Link>
-          <Link
-            href="#contact"
-            onClick={() => {
-              setActiveNav("#contact");
-            }}
-          >
-            Contact
-          </Link>
+        <div className="menu text-light flex justify-center gap-2 lg:gap-3 items-center">
+          <Link className="hidden lg:flex" href="#home">Home</Link>
+          <Link href="#about">About</Link>
+          <Link href="#projects">Project</Link>
+          <Link href="#contact">Contact</Link>
 
           <ThemeSwitcher />
         </div>
